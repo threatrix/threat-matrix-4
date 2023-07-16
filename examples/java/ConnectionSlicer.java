8017a2ddd275344c61e595cd6a204a2cd8cb40ae
jardevbox <34051184+jardevbox@users.noreply.github.com>
2023-07-16 16:35:50 -0400

    update

--- examples/java/ConnectionSlicer.java
+++ examples/java/ConnectionSlicer.java
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v2.7.13
+*    Source File: Connection.java
+*    
+*    Copyrights:
+*      copyright 2012-2021 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package io.threatrix.threatmatrix.slice;
 
 import io.threatrix.common.component.ComponentRevisedLicenseDAO;
