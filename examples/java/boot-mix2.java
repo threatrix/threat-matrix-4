b542c3915d1595c6eeff7e3500e1d7e5bb5244a5
jardevbox <34051184+jardevbox@users.noreply.github.com>
2023-07-16 16:35:47 -0400

    update

--- examples/java/boot-mix2.java
+++ examples/java/boot-mix2.java
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v1.5.3.RELEASE
+*    Source File: RunCommand.java
+*    
+*    Copyrights:
+*      copyright 2012-2016 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 // spring-projects/spring-boot/blob/v2.6.4/spring-boot-project/spring-boot-cli/src/main/java/org/springframework/boot/cli/command/run/SpringApplicationRunner.java
 SpringApplicationRunner(SpringApplicationRunnerConfiguration configuration, String[] sources, String... args) {
         this.configuration = configuration;
